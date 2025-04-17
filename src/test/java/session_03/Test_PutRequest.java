f06ce82d8cbeae55c1bb618c6a9dd2127eb5cede
unnikrishnanbiju <unni1995biju@gmail.com>
2025-04-17 19:20:21 +0530

    updated commit2

--- src/test/java/session_03/Test_PutRequest.java
+++ src/test/java/session_03/Test_PutRequest.java
@@ -5,23 +5,20 @@ import io.restassured.http.ContentType;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
-public class Test_PostRequest {
+public class Test_PutRequest {
 
     @Test
-    public void test04()
-    {
-        JSONObject jsonData = new JSONObject();
-        jsonData.put("name","Unni");
-        jsonData.put("job","QA");
+    public void test04(){
 
-        RestAssured.baseURI= "https://reqres.in/api/users";
+        JSONObject jsonData = new JSONObject();
+        jsonData.put("name","Krishna");
+        jsonData.put("job","Artist");
+
+        RestAssured.baseURI= "https://reqres.in/api/users/680";
         RestAssured.given().header("Content-type", "application/json").contentType(ContentType.JSON).
                 body(jsonData.toJSONString())
-                .when().post()
-                        .then().statusCode(201).log().all();
-
+                .when().put()
+                .then().statusCode(200).log().all();
 
     }
-
-
 }
